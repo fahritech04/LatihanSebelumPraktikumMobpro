5da5ea18f349591c4c3948dae611a4da8e1fd9c1
fahritech04 <fahricoding@gmail.com>
2024-03-13 07:08:56 +0700

    commit Bangun Datar

--- app/src/main/java/com/example/latihansebelumpraktikummobpro/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/latihansebelumpraktikummobpro/ui/screen/MainScreen.kt
@@ -1,51 +1,38 @@
 package com.example.latihansebelumpraktikummobpro.ui.screen
 
 import android.content.res.Configuration
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.latihansebelumpraktikummobpro.R
 import com.example.latihansebelumpraktikummobpro.ui.theme.LatihanSebelumPraktikumMobproTheme
-import kotlin.math.pow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -69,20 +56,15 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
-    var berat by remember { mutableStateOf("") }
-    var beratError by remember { mutableStateOf(false) }
+    var length by remember { mutableStateOf("") }
+    var lengthError by remember { mutableStateOf(false) }
 
-    var tinggi by remember { mutableStateOf("") }
-    var tinggiError by remember { mutableStateOf(false) }
+    var width by remember { mutableStateOf("") }
+    var widthError by remember { mutableStateOf(false) }
 
-    val radioOptions = listOf(
-        stringResource(id = R.string.pria),
-        stringResource(id = R.string.wanita)
-    )
-    var gender by remember { mutableStateOf(radioOptions[0]) }
-
-    var bmi by remember { mutableFloatStateOf(0f) }
-    var kategori by remember { mutableIntStateOf(0) }
+    var area by remember { mutableStateOf(0.0) }
+    var perimeter by remember { mutableStateOf(0.0) }
+    var showError by remember { mutableStateOf(false) }
 
     Column(
         modifier = modifier.fillMaxSize()
@@ -92,17 +74,16 @@ fun ScreenContent(modifier: Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = stringResource(id = R.string.bmi_intro),
+            text = stringResource(id = R.string.bangundatar_intro),
             style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.fillMaxWidth()
         )
         OutlinedTextField(
-            value = berat,
-            onValueChange = { berat = it },
-            label = { Text(text = stringResource(R.string.berat_badan)) },
-            isError = beratError,
-            trailingIcon = { IconPicker(beratError, "kg") },
-            supportingText = { ErrorHint(beratError) },
+            value = length,
+            onValueChange = { length = it },
+            label = { Text(text = stringResource(R.string.length)) },
+            isError = lengthError,
+            supportingText = { ErrorHint(lengthError) },
             singleLine = true,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number,
@@ -111,12 +92,11 @@ fun ScreenContent(modifier: Modifier) {
             modifier = Modifier.fillMaxWidth()
         )
         OutlinedTextField(
-            value = tinggi,
-            onValueChange = { tinggi = it },
-            label = { Text(text = stringResource(R.string.tinggi_badan)) },
-            isError = tinggiError,
-            trailingIcon = { IconPicker(tinggiError, "cm") },
-            supportingText = { ErrorHint(tinggiError) },
+            value = width,
+            onValueChange = { width = it },
+            label = { Text(text = stringResource(R.string.width)) },
+            isError = widthError,
+            supportingText = { ErrorHint(widthError) },
             singleLine = true,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number,
@@ -124,106 +104,48 @@ fun ScreenContent(modifier: Modifier) {
             ),
             modifier = Modifier.fillMaxWidth()
         )
-        Row(
-            modifier = Modifier
-                .padding(top = 6.dp)
-                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
-        ) {
-            radioOptions.forEach { text ->
-                GenderOption(
-                    label = text,
-                    isSelected = gender == text,
-                    modifier = Modifier
-                        .selectable(
-                            selected = gender == text,
-                            onClick = { gender = text },
-                            role = Role.RadioButton
-                        )
-                        .weight(1f)
-                        .padding(16.dp)
-                )
-            }
-        }
+
         Button(
             onClick = {
-                beratError = (berat == "" || berat == "0")
-                tinggiError = (tinggi == "" || tinggi == "0")
-                if (beratError || tinggiError) return@Button
+                lengthError = (length == "" || length == "0")
+                widthError = (width == "" || width == "0")
 
-                bmi = hitungBmi(berat.toFloat(), tinggi.toFloat())
-                kategori = getKategori(bmi, gender == radioOptions[0])
+                if (lengthError || widthError) return@Button
+
+                showError = false
+                area = length.toDouble() * width.toDouble()
+                perimeter = 2 * (length.toDouble() + width.toDouble())
             },
             modifier = Modifier.padding(top = 8.dp),
-            contentPadding = PaddingValues(horizontal=32.dp, vertical=16.dp)
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
         ) {
-            Text(text = stringResource(R.string.hitung))
+            Text(text = stringResource(id = R.string.calculate))
         }
 
-        if (bmi != 0f) {
+
+
+        if (area != 0.0 || perimeter != 0.0) {
             Divider(
                 modifier = Modifier.padding(vertical = 8.dp),
-                thickness = 1.dp
+                thickness = 2.dp
             )
             Text(
-                text = stringResource(R.string.bmi_x, bmi),
+                text = stringResource(R.string.area_x, area),
                 style = MaterialTheme.typography.titleLarge
             )
             Text(
-                text = stringResource(kategori).uppercase(),
-                style = MaterialTheme.typography.headlineLarge
+                text = stringResource(R.string.perimeter_x, perimeter),
+                style = MaterialTheme.typography.titleLarge
             )
         }
-    }
-}
 
-@Composable
-fun GenderOption(label: String, isSelected: Boolean, modifier: Modifier) {
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        RadioButton(selected = isSelected, onClick = null)
-        Text(
-            text = label,
-            style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier.padding(start = 8.dp)
-        )
-    }
-}
-
-@Composable
-fun IconPicker(isError: Boolean, unit: String) {
-    if (isError) {
-        Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
-    } else {
-        Text(text = unit)
     }
 }
 
 @Composable
 fun ErrorHint(isError: Boolean) {
     if (isError) {
-        Text(text = stringResource(R.string.input_invalid))
-    }
-}
-
-private fun hitungBmi(berat: Float, tinggi: Float): Float {
-    return berat / (tinggi / 100).pow(2)
-}
-
-private fun getKategori(bmi: Float, isMale: Boolean): Int {
-    return if (isMale) {
-        when {
-            bmi < 20.5 -> R.string.kurus
-            bmi >= 27.0 -> R.string.gemuk
-            else -> R.string.ideal
-        }
-    } else {
-        when {
-            bmi < 18.5 -> R.string.kurus
-            bmi >= 25.0 -> R.string.gemuk
-            else -> R.string.ideal
-        }
+        Text(text = stringResource(R.string.input_error))
     }
 }
 
